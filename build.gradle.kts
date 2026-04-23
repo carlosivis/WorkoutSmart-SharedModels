9cd58b29c56bd3f68d0971f2b228c607f280fb07
Carlos Ivis <carlosivis1@gmail.com>
2026-04-23 19:33:49 -0300

    Initial project setup for WorkoutSmart-SharedModels

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.serialization)
+    `maven-publish`
+}
+
+
+kotlin {
+    jvm()
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets{
+        commonMain.dependencies{
+            implementation(libs.kotlinx.serialization.json)
+        }
+    }
+}
